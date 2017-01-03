8ffee874fc20b66182915b8ec0c00bf98d89ad96
Zoltan Altfatter <altfatterz@gmail.com>
2017-01-03 23:03:27 +0100

    almost working

--- copy-to-s3.sh
+++ copy-to-s3.sh
@@ -4,4 +4,8 @@ mvn clean package
 aws s3 cp target/cloudwatch-logs-demo-0.0.1.jar s3://spring-boot-examples/cloudwatch-logs-demo/
 aws s3 cp logback.xml s3://spring-boot-examples/cloudwatch-logs-demo/
 aws s3 cp cloudwatch-logs-demo.conf s3://spring-boot-examples/cloudwatch-logs-demo/
+aws s3 cp awslogs/awscli.conf s3://spring-boot-examples/cloudwatch-logs-demo/
+aws s3 cp awslogs/awslogs.conf s3://spring-boot-examples/cloudwatch-logs-demo/
+
+
 
--- instance-launch.sh
+++ instance-launch.sh
@@ -3,6 +3,9 @@
 # update
 yum update -y
 
+# install awslogs
+yum install awslogs -y
+
 # install java 8
 yum install java-1.8.0 -y
 
@@ -16,6 +19,8 @@ mkdir /opt/cloudwatch-logs-demo
 aws s3 cp s3://spring-boot-examples/cloudwatch-logs-demo/cloudwatch-logs-demo-0.0.1.jar /opt/cloudwatch-logs-demo/cloudwatch-logs-demo.jar --region=eu-central-1
 aws s3 cp s3://spring-boot-examples/cloudwatch-logs-demo/logback.xml /opt/cloudwatch-logs-demo/ --region=eu-central-1
 aws s3 cp s3://spring-boot-examples/cloudwatch-logs-demo/cloudwatch-logs-demo.conf /opt/cloudwatch-logs-demo/ --region=eu-central-1
+aws s3 cp s3://spring-boot-examples/cloudwatch-logs-demo/awscli.conf /etc/awslogs/awscli.conf --region=eu-central-1
+aws s3 cp s3://spring-boot-examples/cloudwatch-logs-demo/awslogs.conf /etc/awslogs/awslogs.conf --region=eu-central-1
 
 # create a springboot user to run the app as a service
 useradd springboot
@@ -29,11 +34,13 @@ chmod 500 /opt/cloudwatch-logs-demo/cloudwatch-logs-demo.jar
 # create a symbolic link
 ln -s /opt/cloudwatch-logs-demo/cloudwatch-logs-demo.jar /etc/init.d/cloudwatch-logs-demo
 
-# start the cloudwatch-logs-demo
+# start the cloudwatch-logs-demo and awslogs
 service cloudwatch-logs-demo start
+service awslogs start
 
-# automatically start httpd and spring-boot-ec2-demo if this ec2 instance reboots
+# automatically start and cloudwatch-logs-demo and awslogs if this ec2 instance reboots
 chkconfig cloudwatch-logs-demo on
+chkconfig awslogs on
 
 
 
